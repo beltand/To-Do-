deeb09534bbda36cc00d83a226cf66135e8ca06a
beltand <103342055+beltand@users.noreply.github.com>
2022-06-25 23:05:16 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#add-list');
+const input = document.querySelector('#list-item');
+const todoList = document.querySelector('#list');
+
+todoList.addEventListener('click',function(e){
+    if(e.target.tagName === 'BUTTON'){
+        e.target.parentElement.remove()
+    };
+    if(e.target.tagName === 'INPUT'){
+        e.target.parentElement.style.setProperty('text-decoration', 'line-through');
+    }
+});
+
+//todoList.addEventListener('click', function(e){
+    //if(e.target.tagName === 'INPUT'){
+     //   e.target.parentElement.textDecoration 
+    //}
+//})
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const newItem = document.createElement('li');
+    //const checkDone = document.createElement('input');
+    //checkDone.type = 'checkbox';
+    const removeBtn = document.createElement('button');
+    const checkDone = document.createElement('input');
+    checkDone.type = 'checkbox';
+    removeBtn.innerText = 'Remove';
+    newItem.innerText = input.value;
+    newItem.prepend(checkDone);
+    newItem.appendChild(removeBtn);
+    //newItem.prependChild(checkDone);
+    todoList.appendChild(newItem);
+    input.value = '';
+});
+
